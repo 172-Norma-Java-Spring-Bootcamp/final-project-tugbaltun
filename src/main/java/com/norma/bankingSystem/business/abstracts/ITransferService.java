6ef92da1262e3d95a284b11c3bcb869143a0b0a8
Tugba Altun <36510900+tugbaltun@users.noreply.github.com>
2022-06-06 12:49:58 +0300

    TransferManagement

---
+++ src/main/java/com/norma/bankingSystem/business/abstracts/ITransferService.java
@@ -0,0 +1,9 @@
+package com.norma.bankingSystem.business.abstracts;
+
+import com.norma.bankingSystem.entity.model.Recipient;
+
+public interface ITransferService {
+    void betweenAccountsTransfer(String transferFrom, String transferTo, String amount) throws Exception;
+
+    //void toSomeoneElseTransfer(Recipient recipient, String accountType, String amount);
+}
